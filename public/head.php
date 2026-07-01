5eddc97e308ed725582d723f9d5104b28b605280
Saicharan Bhuthkuri <saicharanbhuthkuri468@gmail.com>
2026-07-01 10:39:09 +0530

    Add Google site verification to head.php
    Added Google site verification meta tag.

--- public/head.php
+++ public/head.php
@@ -3,3 +3,4 @@
 <link rel="stylesheet" href="css/style.css">
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
 <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
+<meta name="google-site-verification" content="fyPYBMa7M8fenxfEgxl8JdZHFZmQbqEBGeL7pagy2Ew" />
